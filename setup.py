eb5b1d5f6c8b7fdcd8af18a4e8b665732665f6a6
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-08-18 21:46:35 -0400

    fix: setup.py not allowing 3.12 to be built

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ setup(
         "Topic :: System :: Networking",
     ],
     url="https://github.com/thegamecracks/link-python",
-    python_requires=">=3.6,<3.12",
+    python_requires=">=3.6,<3.13",
     include_package_data=True,
     ext_modules=[CMakeExtension("LinkPython-extern")],
     cmdclass={"build_ext": CMakeBuild},
