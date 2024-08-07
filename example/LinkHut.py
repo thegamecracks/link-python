e79bb3e038951233cab136f86a5d968b3544bf1d
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-08-06 21:44:46 -0400

    feat: tweak LinkHut.py to more closely mimic official example
    https://github.com/Ableton/link/blob/Link-3.1.2/examples/linkhut/main.cpp

--- example/LinkHut.py
+++ example/LinkHut.py
@@ -9,18 +9,28 @@ def main() -> None:
     l.enabled = True
     l.startStopSyncEnabled = True
 
+    print("enabled | num peers | quantum | start stop sync | tempo   | beats   | metro")
+
     while True:
         s = l.captureSessionState()
 
         link_time = l.clock().micros()
+        enabled = "yes" if l.enabled else "no"
+        peers = l.numPeers()
+        start_stop_sync = "yes" if l.startStopSyncEnabled else "no"
+        playing = "[playing]" if s.isPlaying() else "[stopped]"
         beats = s.beatAtTime(link_time, 0)
-        phase = int(s.phaseAtTime(link_time, QUANTUM))
-        phase = "X" * phase + "0" * (QUANTUM - phase)
+        phase = int(s.phaseAtTime(link_time, QUANTUM)) + 1
+        metro = "X" * phase + "0" * (QUANTUM - phase)
 
         print(
-            f"tempo {s.tempo():.2f} | "
-            f"playing: {s.isPlaying()} | "
-            f"beats {beats:.2f} | {phase}",
+            f"{enabled:<7s} | "
+            f"{peers:<9d} | "
+            f"{QUANTUM:<7d} | "
+            f"{start_stop_sync:<3s} {playing:<11s} | "
+            f"{s.tempo():<7.2f} | "
+            f"{beats:<7.2f} | "
+            f"{metro}",
             end="\r",
             flush=True,
         )
