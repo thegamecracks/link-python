07ef5f56f3a65d07481de196590447345ce6dc4c
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-08-06 21:34:54 -0400

    refactor: clean up LinkHut.py

--- example/LinkHut.py
+++ example/LinkHut.py
@@ -1,36 +1,35 @@
-#!/usr/bin/env python3
-
-import os
-import sys
+import link
 import time
 
-lib_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + '/../lib/')
-sys.path.insert(0, lib_dir)
+QUANTUM = 4  # beats per bar
 
-import link
 
-l = link.Link(120)
-l.enabled = True
-l.startStopSyncEnabled = True
+def main() -> None:
+    l = link.Link(120)
+    l.enabled = True
+    l.startStopSyncEnabled = True
 
-try:
-  while True:
-    s = l.captureSessionState()
-    link_time = l.clock().micros()
-    tempo_str = '{0:.2f}'.format(s.tempo())
-    beats_str = '{0:.2f}'.format(s.beatAtTime(link_time, 0))
-    playing_str = str(s.isPlaying())
-    phase = s.phaseAtTime(link_time, 4)
-    phase_str = ''
-    for x in range(0, 4):
-      if x < phase:
-        phase_str += 'X'
-      else:
-        phase_str += '0'
-    sys.stdout.write(
-      'tempo ' + tempo_str + ' | playing: ' + playing_str + ' | beats ' + beats_str
-      + ' | ' + phase_str + '  \r')
-    sys.stdout.flush()
-    time.sleep(0.02)
-except KeyboardInterrupt:
-    pass
+    while True:
+        s = l.captureSessionState()
+
+        link_time = l.clock().micros()
+        beats = s.beatAtTime(link_time, 0)
+        phase = int(s.phaseAtTime(link_time, QUANTUM))
+        phase = "X" * phase + "0" * (QUANTUM - phase)
+
+        print(
+            f"tempo {s.tempo():.2f} | "
+            f"playing: {s.isPlaying()} | "
+            f"beats {beats:.2f} | {phase}",
+            end="\r",
+            flush=True,
+        )
+
+        time.sleep(0.02)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
